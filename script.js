25cf204c8202c1281e106d5b84f318321458e20d
Dishan Selvanantham <e227493@edu.sbl.ch>
2023-10-31 11:11:42 +0100

    aufgabe 1, 2

--- script.js
+++ script.js
@@ -4,7 +4,38 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+    if (currentElement === "e") {
+     // result.push("")
+    }
+    else if(currentElement === "E") {
+      //result.push("")
+    }
+   else {
     result.push(currentElement)
+   } 
+  }
+  return result.join("")
+}
+
+export function aufgabe02 (args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    let currentElement = input[i]
+    currentElement = currentElement.toUpperCase()
+    result.push(currentElement)
+  
+  }
+  
+  return result.join("")
+}
+
+export function aufgabe03 (args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    
   }
   return result.join("")
 }
