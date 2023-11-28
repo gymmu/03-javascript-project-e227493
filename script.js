1015b7ddc7f3b26d10637338e5267751ae8eb09a
Dishan Selvanantham <e227493@edu.sbl.ch>
2023-11-28 10:22:01 +0100

    Aufgabe 3,4 gelöst

--- script.js
+++ script.js
@@ -33,9 +33,61 @@ export function aufgabe02 (args) {
 export function aufgabe03 (args) {
   const input = args
   const result = []
+  let count = 0
+
   for (let i = 0; i < input.length; i++) {
-    const currentElement = input[i]
-    
+    const currentElement = input [i]
+    if (currentElement === "e") {
+      count++
+    }
+    else if (currentElement === "E") {
+      count++
+    }
   }
-  return result.join("")
+  
+  return count
+
+  }
+
+  export function aufgabe04 (args) {
+    const input = args
+    const result = []
+    let count = 1
+    for (let i = 0; i < input.length; i++) {
+      const currentElement = input [i]
+      if (currentElement === " ") {
+        count++
+      }
+}
+    return count
+  }
+
+  export function aufgabe05 (args) {
+    const input = args
+    const result = []
+    
+    return result.join("")
+  }
+
+export function aufgabe10 (args) {
+  const input = args
+  
+  if (input.length !== 7 ) return false
+  if (input[0] !== "#") return false
+
+  for (let i =1; i < input.length; i++) {
+    const currentElement = input [i]
+    const ascii = currentElement.charCodeAt (0)
+    
+    if (48 <= ascii && ascii <= 57) {
+      //Ist eine Ziffer
+
+    } else if (65 <= ascii && ascii <= 70) {
+    //Ist A-F
+  } else {
+    return false
+  }
+  }
+
+  return true
 }
